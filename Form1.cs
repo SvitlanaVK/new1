6aa99b0e1aeade546a1b227b4e136c9361ec99d3
svk <svitlanakornilova@gmail.com>
2017-11-16 00:24:53 +0200

    начальній коммит

---
+++ Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Configuration;
+
+namespace kusrovikdb
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Form form2;
+            form2 = new Form2();
+            form2.Show();
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Form form3 = new Form3();
+            form3.Show();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Form form4 = new Form4();
+            form4.Show();
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            Form form5 = new Form5();
+            form5.Show();
+        }
+
+        private void Form1_Shown(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
